366bd22427c7467eac51e83425b59dfb7132e13f
kotpilun <kotpilun3m@gmail.com>
2017-11-05 01:26:39 +0300

    done

---
+++ W02_01.py
@@ -0,0 +1,28 @@
+import pandas as pn
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+
+data = pn.read_csv('/home/oleg/PycharmProjects/ML-Found-Yandex/Data/Wine2.data')
+y = data.res
+del data['res']
+
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+#accuracy = {}
+acc = list()
+
+for i in range(1,51):
+    neigh = KNeighborsClassifier(i)
+    acc.append(cross_val_score(neigh, data, y, cv=kf).mean())
+)
+# print(pn.Series(acc).sort_values(ascending=False))
+acc = list()
+data = preprocessing.scale(data)
+for i in range(1,51):
+    neigh = KNeighborsClassifier(i)
+    acc.append(cross_val_score(neigh, data, y, cv=kf).mean())
+
+# print(pn.Series(acc))
+print(pn.Series(acc).sort_values(ascending=False))
